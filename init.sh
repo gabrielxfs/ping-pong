51cfdc62a284fa6a2405c97c0394e9aa514b9f7f
gabrielxfs <gabriel.silva.w1n3@gmail.com>
2024-03-27 17:24:10 -0300

    Proper dummy server initialization

--- init.sh
+++ init.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-echo "Waiting for ${TARGET}..."
-./wait-for-it.sh $TARGET:80 -t 0
+exec node /app/server.js $IN &
+
+echo "Waiting for ${TARGET} on ${OUT}..."
+./wait-for-it.sh $TARGET:$OUT -t 0
 
 ping $OPTS $TARGET
